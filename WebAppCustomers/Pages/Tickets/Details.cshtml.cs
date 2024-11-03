3842d36b36de40c4abac94db5faccd37993c9ef7
Julio Guzman <juligu@microsoft.com>
2024-11-03 16:38:28 -0500

    Details

---
+++ WebAppCustomers/Pages/Tickets/Details.cshtml.cs
@@ -0,0 +1,13 @@
+
+
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebAppCustomers.Pages.Tickets;
+
+public class DetailsModel : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
