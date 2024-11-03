b9f2d500c507c7649908d7b37f998b6e6276269f
Julio Guzman <juligu@microsoft.com>
2024-11-03 18:05:08 -0500

    Test Helper

---
+++ WebAppCustomers/Helpers/TestHelper.cs
@@ -0,0 +1,25 @@
+namespace WebAppCustomers.Helpers
+{
+    public static class TestHelper
+    {
+        public static int Factorial(int n)
+        {
+            if (n == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return n * Factorial(n - 1);
+            }
+        }
+
+        // reverse a string
+        public static string Reverse(string s)
+        {
+            char[] charArray = s.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+    }
+}
